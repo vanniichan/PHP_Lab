1e594881dc2eca09a71d79b3700adb7e31df5ddc
TwentySeV <112863484+vanniichan@users.noreply.github.com>
2023-12-19 22:16:01 +0700

    Add files via upload

---
+++ Lab_4/Bai_2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload an Image</title>
+</head>
+<body>
+    <h2>Upload an Image</h2>
+    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>" enctype="multipart/form-data">
+        <input type="file" name="file" id="file" required><br>
+
+        <input type="submit" name = "file" value="Upload File">
+    </form>
+<style>
+    body{
+        text-align: center;
+    }
+</style>
+</body>
+</html>
+
+
+<?php
+$uploadDir = "uploads/";
+if (!file_exists($uploadDir)) {
+    mkdir($uploadDir, 0777, true);
+}
+$uploadedFile = "";
+
+if (isset($_POST['file'])) {
+    $uploadedFile = $uploadDir . basename($_FILES["file"]["name"]);
+    $uploadOk = true;
+    $imageFileType = strtolower(pathinfo($uploadedFile, PATHINFO_EXTENSION));
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+
+    if (!in_array($imageFileType, $allowedExtensions)) {
+        echo '<script>window.alert("Invalid file type");</script>';
+        $uploadOk = false;
+    }
+
+    if ($uploadOk) {
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $uploadedFile)) {
+            echo "File uploaded successfully.";
+            if (!empty($uploadedFile) && file_exists($uploadedFile)) {
+                echo "<br>";
+                echo "<img src=\"$uploadedFile\">";
+            }
+        } else {
+            echo "Đã xảy ra lỗi.";
+        }
+    } else {
+        echo "File not uploaded.";
+    }
+    
+}
+?>
